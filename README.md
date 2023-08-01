5924f2205e42b579176846520eab7746f20897b5
Sharad-bootcamp <137394854+Sharad-bootcamp@users.noreply.github.com>
2023-08-01 12:24:43 +0930

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # python-api-challenge
 Week 6 Challenge
+Reference and support - https://stackoverflow.com/search?q=api+and+python
+askclassmates on slack
---
+++ starter_code/api_keys.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "8fa3104831d823f57256769225c63a26"
+
+# Geoapify API Key
+geoapify_key = "473472ac91114c5a9a1e3dae01cdb6ba"
