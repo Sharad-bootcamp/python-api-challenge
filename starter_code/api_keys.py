df559b496e3c2a016a18902306b59a1ecc87eb54
Sharad-bootcamp <137394854+Sharad-bootcamp@users.noreply.github.com>
2023-08-01 12:27:21 +0930

    Update api_keys.py

--- starter_code/api_keys.py
+++ starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "8fa3104831d823f57256769225c63a26"
+weather_api_key = "xxxxxxxxxxxxxxxxxxxxxxxx"
 
 # Geoapify API Key
-geoapify_key = "473472ac91114c5a9a1e3dae01cdb6ba"
+geoapify_key = "xxxxxxxxxxxxxxxxxxxxxxxxx"
